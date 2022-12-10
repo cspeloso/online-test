60097b0ce9939ba5679dba48fa093f35c540ba32
Chris Peloso <chris@macbook-air.myfiosgateway.com>
2022-12-10 15:25:22 -0500

    adding git pull

--- pages/assets/gitpull.php
+++ pages/assets/gitpull.php
@@ -1,9 +1,12 @@
 <?php
 
+    $html = "<h1>GitPull Page</h1>
+            <p>This page will pull the latest update from github.</p>";
+
     if($_POST['payload']){
         // error_log('git WEBHOOK received!');
         // shell_exec('cd ~/public_html && git pull origin master');
-        exec('cd ~/public_html && git pull origin main'.' 2>&1', $output, $return_var);
+        exec('cd ~/public_html/online-test && git pull origin main'.' 2>&1', $output, $return_var);
         var_dump($output);
     }
     else {
