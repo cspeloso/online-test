8f74a63b3b2803778a62f37b688fca9c2d3de4cf
Chris Peloso <chris@macbook-air.myfiosgateway.com>
2022-12-10 15:38:37 -0500

    adding git pull

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
 
     </canvas>
 
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src='scripts/script.js'></script>
 
 </body>
