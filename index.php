71d27cf1af58b00de1c9bb30464de0f1938e1c6c
Chris Peloso <chris@macbook-air.myfiosgateway.com>
2022-12-10 15:29:34 -0500

    adding git pull

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Test</title>
+
+    <link rel='stylesheet' href='styles/style.css' />
 </head>
 <body>
     
     <h1>Online Test</h1>
     <p>online game</p>
     <p>test 2</p>
+
+    <canvas id='game_canvas' width='300' height='300'></canvas>
+
+
 </body>
 </html>
