468a77de5e968c5397946b260f490c61ace494dc
Chris Peloso <chris@macbook-air.myfiosgateway.com>
2022-12-10 15:36:04 -0500

    adding git pull

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-function draw() {
-    const canvas = document.getElementById("game_canvas");
+const canvas = document.getElementById('myCanvas');
+const context = canvas.getContext('2d');
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+const radius = 70;
 
-    if (canvas.getContext) {
-      const ctx = canvas.getContext("2d");
-
-      ctx.fillStyle = "rgb(200, 0, 0)";
-      ctx.fillRect(10, 10, 50, 50);
-
-      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-      ctx.fillRect(30, 30, 50, 50);
-    }
-  }
+context.beginPath();
+context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+context.fillStyle = 'green';
+context.fill();
+context.lineWidth = 5;
+context.strokeStyle = '#003300';
+context.stroke();
