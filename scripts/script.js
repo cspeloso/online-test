66aa04b125f1affa1742045b793ee7a12f93f5a5
Chris Peloso <chris@macbook-air.myfiosgateway.com>
2022-12-10 15:37:39 -0500

    adding git pull

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById('game_canvas');
 const context = canvas.getContext('2d');
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
