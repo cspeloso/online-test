f6b3f3923ac636793c82bbe7922b2357b94ea4a5
cspeloso <chrispeloso2@gmail.com>
2022-12-11 17:10:25 -0500

    game update

--- scripts/GameScript.js
+++ scripts/GameScript.js
@@ -3,3 +3,27 @@ var canvas = document.querySelector('#game_canvas');
 var context = canvas.getContext('2d');
 
 context.fillRect(25,25,25,25);
+
+
+
+function CreatePlayer(x,y,width,height,color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+
+    this.radius = 10;
+
+    this.drawPlayer = function(){
+        context.save();
+
+        context.fillStyle = this.color;
+        context.translate(this.x,this.y);
+        context.fillRect((-this.width/2), (-this.height/2), this.width, this.height);
+
+        context.restore();
+    }
+
+}
+
