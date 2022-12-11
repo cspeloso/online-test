ed814a43e0dc643e7a6d1b5f43f9fd35d4429bb8
cspeloso <chrispeloso2@gmail.com>
2022-12-11 18:28:52 -0500

    game update

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,8 @@ var player = new Player(50,100, 50, 50, "red");
 function animate() {
     context.clearRect(0,0,canvas.width, canvas.height);
 
-    if(w && player.top() > 0) player.y -= 8;
-    if(s && player.bottom() < canvas.height) player.y += 8;
+    if(w && player.top() > 0) player.y -= 4;
+    if(s && player.bottom() < canvas.height) player.y += 4;
     if(a && player.left() > 0) player.x -= 8;
     if(d && player.right() < canvas.width) player.x += 8;
 
