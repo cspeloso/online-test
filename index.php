281cb78073c608a849a24f6fb69d9c64d6e0a3fe
cspeloso <chrispeloso2@gmail.com>
2022-12-11 15:55:36 -0500

    creating base game

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src='scripts/script.js'></script>
+    <script src='scripts/controller.js'></script>
 
 </body>
 </html>
