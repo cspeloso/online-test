2556e60934b89eac4895c86cf37c4002eb77f144
cspeloso <chrispeloso2@gmail.com>
2022-12-11 15:54:30 -0500

    creating base game

---
+++ scripts/controller.js
@@ -0,0 +1,30 @@
+var w = false;
+var a = false;
+var s = false;
+var d = false;
+
+$(document).on('keydown', press);
+$(document).on('keyup', release);
+
+
+function press(e){
+    if(e.keyCode == 87)
+        w = true;
+    if(e.keyCode == 83)
+        s = true;
+    if(e.keyCode == 65)
+        a = true;
+    if(e.keyCode == 68) 
+        d = true;    
+}
+
+function release(e) {
+    if(e.keyCode == 87)
+        w = false;
+    if(e.keyCode == 83)
+        s = false;
+    if(e.keyCode == 65)
+        a = false;
+    if(e.keyCode == 68) 
+        d = false;    
+}
--- scripts/script.js
+++ scripts/script.js
@@ -7,35 +7,51 @@ var radius = 70;
 var interval = 1000/60;
 var update = setInterval(animate, interval);
 
-$(document).on('keypress', function(e) {
-
-    //  up
-    if(e.which == 38 || e.which == 119)
-    {
-        context.centerY += 1;
-    }
-
-    //  down
-    if(e.which == 40 || e.which == 115)
-    {
-        context.centerY -= 1;
-    }
-
-    //  left
-    if(e.which == 37 || e.which == 97)
-    {
-        context.centerX -= 1;
-    }
-
-    //  right
-    if(e.which == 39 || e.which == 100)
-    {
-        context.centerX += 1;
-    }
-});
+var player = new GameObject(50,100, 50, 50, "red");
 
 
+function animate() {
+    context.clearRect(0,0,canvas.width, canvas.height);
+    
+    if(w && player.bottom() > canvas.height) player.y -= 8;
+    if(s) player.y += 8;
+    if(a) player.x -= 8;
+    if(d) player.x += 8;
+
+    if(this.bottom > canvas.height)
+        player.y = canvas.height - player.h/2;
+    if(this.top < 0)
+        player.y = 0 + player.h/2;
+    if(this.left < 0)
+        player.x = 0 + player.h/2;
+    if(this.right > canvas.width)
+        player.x = canvas.width - player.w/2;
+
+}
 
 function GameObject(x = canvas.width/2,y = canvas.height/2,w = 50,h = 50,color = "#FF0000") {
     
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+
+    this.radius = 10;
+
+    this.drawRect = function() {
+        context.save();
+        context.fillStyle = this.color;
+        context.translate(this.x, this.y);
+        context.fillRect((-this.w/2), (-this.h/2), this.w, this.h);
+
+        context.restore();
+    }
+
+    this.left = this.x - this.width/2;
+    this.right = this.x + this.width/2;
+
+    this.top = this.y - this.height/2;
+    this.bottom = this.y + height/2;
+
 }
