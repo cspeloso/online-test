95a73ac601db5f69ddf007d1fdbf913f0fbb634c
cspeloso <chrispeloso2@gmail.com>
2022-12-11 19:23:45 -0500

    working on web sockets

--- websockets test/index.php
+++ websockets test/index.php
@@ -11,20 +11,30 @@
     <h1>Welcome to the websockets test</h1>
     <p>This is a websockets test my negro.</p>
 
+    <h3>Chat Area</h3>
+    <div class='chats'>
+        
+    </div>
+
     <script>
+
         const ws = new WebSocket("ws://localhost:8082");
 
+        //  when your connection is made to the server
         ws.addEventListener("open", () => {
             console.log("We're connected!");
 
             ws.send("Hey, how's it going?");
         });
 
-        ws.addEventListener("message", ({ data }) => {
-            console.log(data);
+        //  when you receive a message from the server
+        ws.addEventListener("message", e => {
+            $('.chats').append(`<p class='chat'>${e.data}</p>`);
         });
+
     </script>
 
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 
 </body>
 </html>
